48b385380d1e423d8ecce63e22802f2df7ca4ae2
EugeneRedwing8 <emersonejohnson@gmail.com>
2025-08-11 16:53:37 -0700

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,5 @@
+namespace userconfig {
+    export const ARCADE_SCREEN_WIDTH = 512
+    export const ARCADE_SCREEN_HEIGHT = 512
+}
+let myTextSprite = fancyText.create("Hello im a doot in boot with a floot 1234567890!@#$%^&*()<>?,./\\}{][-_=+`~", 0, 10, customFont.BARRIER_font)
